b3726f9ff77c516394034c29fa84a0c387c59093
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-06-06 15:50:00 +0530

    Roles Update

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,7 +1,7 @@
 export const Bio = {
     name: "Mann Savani",
     image: "https://alpha-stark.github.io/images/MannSavani.jpg",
-    roles: ["Full Stack Developer.", "Programmer."],
+    roles: ["Full Stack Developer.", "Programmer.", "Competitive Coder"],
     description: "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/Alpha-Stark",
     resume: "ABC",
