a451d0fd70e491288db1a752c64e685470b4e269
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-06-08 13:30:46 +0530

    TA Resume Link added

--- src/data/constants.js
+++ src/data/constants.js
@@ -4,7 +4,7 @@ export const Bio = {
     roles: ["Full Stack Developer.", "Programmer.", "Competitive Coder"],
     description: "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/Alpha-Stark",
-    resume: "ABC",
+    resume: "https://drive.google.com/file/d/1UL2V3bPgyHeshuGeEDZEQPmoV22RmEua/view?usp=sharing",
     // resume: "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/mann-savani/",
     twitter: "https://twitter.com/MANNSAVANI2",
